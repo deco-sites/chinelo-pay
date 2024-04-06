86e44cb922b909254f6953f54283c825cf04c908
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-04-06 16:14:46 -0300

    feat: now you can add a benefit list to the main product

--- sections/MainLogo.tsx
+++ sections/MainLogo.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 export default function MainLogo({logoImg, logoImgAlt, logoSubTitle}: Props) {
-  return (<div class="bg-gradient-to-r from-zinc-800 via-zinc-950 to-black px-10 sm:px-16 pt-16 flex flex-col items-center text-white">
+  return (<div class="bg-gradient-to-r from-zinc-800 via-zinc-900 to-black px-10 sm:px-16 pt-16 flex flex-col items-center text-white">
     <div class="inline-block">
       <Image 
         width={715}
@@ -16,8 +16,8 @@ export default function MainLogo({logoImg, logoImgAlt, logoSubTitle}: Props) {
         alt={logoImgAlt}
       />
     </div>
-    <p class="text-justify text-2xl sm:text-3xl font-thin text-zinc-400 py-4">
+    <h2 class="text-justify text-2xl sm:text-3xl font-thin text-zinc-400 py-4">
       {logoSubTitle || ""}
-    </p>
+    </h2>
   </div>)
 }
--- sections/MainProduct.tsx
+++ sections/MainProduct.tsx
@@ -4,16 +4,26 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 interface Props {
   productImage: ImageWidget;
   productImageAlt?: string;
+  benefitsTitle: string;
+  benefits: string[];
 }
 
-export default function MainProduct({productImage, productImageAlt}:Props) {
-  return (<div class="bg-gradient-to-r from-zinc-800 via-zinc-950 to-black px-10 sm:px-16 pt-16 flex items-center text-white">
-    <div class="inline-block">
+export default function MainProduct({productImage, productImageAlt, benefitsTitle, benefits}:Props) {
+  return (<div class="bg-gradient-to-r from-zinc-800 via-zinc-900 to-black px-10 sm:px-16 pt-16 flex flex-col lg:flex-row items-center text-white">
+    <div class="sm:inline-block">
       <Image 
         src={productImage}
         alt={productImageAlt || ""}
         width={1000}
       />
+    </div>
+    <div class="min-w-[410px] min-h-[400px]">
+      <h2 class="text-lg mb-4">{benefitsTitle}</h2>
+      {
+        benefits.map(benefit => (
+          <p class="font-light my-2">{benefit}</p>
+        ))
+      }
     </div>
   </div>)
 }
