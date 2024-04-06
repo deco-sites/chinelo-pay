d81a310987bf84737317cb51501d7131058f668d
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-04-06 15:33:05 -0300

    feat: now you can add a image to the main product

--- sections/MainProduct.tsx
+++ sections/MainProduct.tsx
@@ -1,5 +1,19 @@
-export default function MainProduct() {
+import Image from "apps/website/components/Image.tsx";
+import type { ImageWidget } from "apps/admin/widgets.ts";
+
+interface Props {
+  productImage: ImageWidget;
+  productImageAlt?: string;
+}
+
+export default function MainProduct({productImage, productImageAlt}:Props) {
   return (<div class="bg-gradient-to-r from-zinc-800 via-zinc-950 to-black px-10 sm:px-16 pt-16 flex items-center text-white">
-    <p>TESTE</p>
+    <div class="inline-block">
+      <Image 
+        src={productImage}
+        alt={productImageAlt || ""}
+        width={1000}
+      />
+    </div>
   </div>)
 }
